6c6df604df6f5e5e1e35a74c4665bdbb8e27d5b2
kali_b <kalibrayfield@gmail.com>
2020-08-04 17:38:40 -0500

    Added project training days

--- number-guesser/script.js
+++ number-guesser/script.js
@@ -2,5 +2,7 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
-
+const generateTarget =  +> {
+	const randomNumber = Math.floor(Math.random() * 10);
+};
+console.log(randomNumber);
